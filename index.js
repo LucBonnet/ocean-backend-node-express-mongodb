955c1e8e9e01452e4957a17109c8b86c819589e4
LucBonnet <lucbonnet10@gmail.com>
2021-07-07 12:05:25 -0300

    read all e read single

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World')
-})
+const lista = ["Bulbasaur", "Charmander", "Squirtle"];
 
-app.listen(3000)
+// [POST]      -> create
+// [GET]       -> read
+// [PUT/PATCH] -> update
+// [DELETE]    -> delete
+
+// [GET] -> Read All
+app.get("/pokemons", (req, res) => {
+  res.send(lista);
+});
+
+// [GET] -> Read Single (by id)
+app.get("/pokemons/:id", (req, res) => {
+  const id = req.params.id;
+
+  const item = lista[id - 1];
+
+  res.send(item);
+});
+
+app.post("/pokemons", (req, res) => {
+  lista.push();
+});
+
+app.listen(3000);
