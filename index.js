5ab07e1c180eedc7ef1ccea986a74fe86b2b6bef
LucBonnet <lucbonnet10@gmail.com>
2021-07-07 13:01:06 -0300

    CRUD completo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// informa que o corpo de requisição será em JSON
+app.use(express.json());
+
 const lista = ["Bulbasaur", "Charmander", "Squirtle"];
 
 // [POST]      -> create
@@ -10,7 +13,7 @@ const lista = ["Bulbasaur", "Charmander", "Squirtle"];
 
 // [GET] -> Read All
 app.get("/pokemons", (req, res) => {
-  res.send(lista);
+  res.status(200).send(lista.filter(Boolean));
 });
 
 // [GET] -> Read Single (by id)
@@ -19,11 +22,34 @@ app.get("/pokemons/:id", (req, res) => {
 
   const item = lista[id - 1];
 
-  res.send(item);
+  res.status(200).send(item);
 });
 
+// [POST] - Create
 app.post("/pokemons", (req, res) => {
-  lista.push();
+  const item = req.body.nome;
+
+  lista.push(item);
+
+  res.status(201).send("Criado com sucesso");
+});
+
+// [PUT] - Update
+app.put("/pokemons/:id", (req, res) => {
+  const id = req.params.id;
+  const item = req.body.nome;
+  lista[id - 1] = item;
+
+  res.send("Item alterado com sucesso");
+});
+
+// [DELETE] - Delete
+app.delete("/pokemons/:id", (req, res) => {
+  const id = req.params.id;
+
+  delete lista[id - 1];
+
+  res.send("Item removido com sucesso");
 });
 
 app.listen(3000);
